c1d92316b3f389e7ba7d52b9184c7e91aeec29d8
Ripa Sarkar <ripasarkar49@gmail.com>
2025-12-06 22:28:32 +0600

    vercel file creat

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
 
-    await client.connect();
+    // await client.connect();
     const database=client.db('petservice');
     const petServices=database.collection('services')
     const orderCollection=database.collection('orders')
@@ -115,7 +115,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
